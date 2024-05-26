89e732c54054e7cda460974de9d6cbf4c312ba45
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-27 00:54:19 +0530

    update: image upload profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,44 +1,109 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect, useCallback } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
 
 const Profile = () => {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  const handleFileUpload = useCallback((file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        console.log(error);
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData((prevFormData) => ({ ...prevFormData, avatar: downloadURL }))
+        );
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file, handleFileUpload]);
+
   return (
-    <div className="max-w-lg p-3 mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+    <div className='max-w-lg p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
         <img
-          src={currentUser.avatar}
-          alt="profile"
-          className="self-center object-cover w-24 h-24 mt-2 rounded-full cursor-pointer"
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
+          className='self-center object-cover w-24 h-24 mt-2 rounded-full cursor-pointer'
+        />
+        <p className='self-center text-sm'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+        <input
+          type='text'
+          placeholder='username'
+          id='username'
+          className='p-3 border rounded-lg'
         />
         <input
-          type="text"
-          placeholder="username"
-          id="username"
-          className="p-3 border rounded-lg"
+          type='email'
+          placeholder='email'
+          id='email'
+          className='p-3 border rounded-lg'
         />
         <input
-          type="email"
-          placeholder="email"
-          id="email"
-          className="p-3 border rounded-lg"
+          type='password'
+          placeholder='password'
+          id='password'
+          className='p-3 border rounded-lg'
         />
-        <input
-          type="text"
-          placeholder="password"
-          id="password"
-          className="p-3 border rounded-lg"
-        />
-        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
+        <button className='p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80'>
           update
         </button>
       </form>
-      <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
     </div>
   );
-};
-
+}
 export default Profile;
