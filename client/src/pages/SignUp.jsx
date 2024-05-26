445ed248aec80f9d2099304bb22b73082755f11d
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-26 15:43:09 +0530

    update: Header , database connect and user model, sign-up api route, middleware

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+
+
+const SignUp = () => {
+  return (
+    <div>
+      Signout
+    </div>
+  )
+}
+
+export default SignUp
---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  console.log(req.body);
+  const { username, phonenumber, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+    phonenumber,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!!!");
+  } catch (error) {
+    next(error);
+  }
+};
