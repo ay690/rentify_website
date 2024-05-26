4ea00a05004798f682a1fddaf6e1c98a08ac2589
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-26 17:18:25 +0530

    update: signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   console.log(req.body);
@@ -15,6 +16,29 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!!!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password, phonenumber } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const validPhoneNumber = await User.findOne({ phonenumber });
+    if (!validPhoneNumber) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
