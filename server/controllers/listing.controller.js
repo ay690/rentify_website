3fc91be9317cec0fcd1c0989228f08bba75c3e1a
Aniket Yadav <aniketyadav690@gmail.com>
2024-05-30 20:56:51 +0530

    update: api for deleting user listing

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,9 +1,34 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.param.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Lsitings not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(
+        401,
+        "You are not authorized or you can delete only your listings!!!"
+      )
+    );
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.param.id);
+    res.status(201).json("Listing has been deleted!!!");
   } catch (error) {
     next(error);
   }
