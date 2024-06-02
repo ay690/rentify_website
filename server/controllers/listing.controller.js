1896565f046b65f28186d793fb1a69d98dd7e8c9
Aniket Yadav <aniketyadav690@gmail.com>
2024-06-02 14:59:58 +0530

    update: get listing api route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -58,3 +58,15 @@ export const updateListings = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) return next(errorHandler(404, "No listings found!!!"));
+
+    res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   createListing,
   deleteListing,
   updateListings,
+  getListings,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListings);
+router.get("/get/:id", getListings);
 
 export default router;
