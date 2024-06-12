655b9187a57027bb26fa0c9ff16e6aef8da3a3f9
Aniket Yadav <aniketyadav690@gmail.com>
2024-06-12 15:38:32 +0530

    feat: About Us Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,32 @@
-
-
 const About = () => {
   return (
-    <div>
-      About 
+    <div className="max-w-6xl px-4 py-20 mx-auto">
+      <h1 className="mb-4 text-3xl font-bold text-slate-700 md:text-center">
+        About Rentify
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Rentify is a premier real estate agency specializing in assisting
+        clients with buying, selling, and renting properties in highly
+        sought-after neighborhoods. Our team of seasoned agents is committed to
+        delivering outstanding service and ensuring a seamless experience
+        throughout the entire buying and selling process.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to assist clients in reaching their real estate
+        objectives by offering expert guidance, tailored service, and
+        comprehensive knowledge of the local market. Whether you&#39;re buying,
+        selling, or renting, we are dedicated to supporting you through every
+        step of the process.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents brings extensive experience and deep knowledge of the
+        real estate industry, dedicated to delivering top-notch service to our
+        clients. We believe that buying or selling a property should be a
+        thrilling and fulfilling experience, and we are committed to making this
+        a reality for every client we serve.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
